0bc6ed13bb340eede20cb627996db88ab11838bb
ZXC_DeSh <egorkorobkin37@gmail.com>
2024-08-28 14:37:49 +0300

    git commit -m(ne nado)

---
+++ GIT/advertisment/app_advertisment/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from .models import Advertisement
+from django.contrib.auth.decorators import login_required
+from .forms import AdvertisementForm
+from django.urls import reverse,reverse_lazy
+from django.contrib.auth import get_user_model
+from django.db.models import Count
+
+User = get_user_model()
+
+def index(request):
+    title = request.GET.get('query')
+    if title:
+        advertisements = Advertisement.objects.filter(title__icontains=title)
+    else:
+        advertisements = Advertisement.objects.all()
+    context = {'advertisements': advertisements, 
+               "title":title,
+               }
+    return render(request, 'adv/index.html', context)
+
+def top_sellers(request):
+    users = User.objects.annotate(
+        adv_count = Count('advertisement')
+    ).order_by('-adv_count')
+    context = {
+        'users':users
+    }
+    return render(request, 'adv/top-sellers.html', context)
+
+@login_required(login_url=reverse_lazy('login'))
+def advertisement_post(request):
+    if request.method == 'POST':
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisement(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('main_page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    context = {'form': form}
+    return render(request, 'adv/advertisement_post.html', context)
+
+def advertisement_detail(request, pk):
+    advertisement = Advertisement.objects.get(id=pk)
+    context = {
+        'advertisement': advertisement
+    }
+    return render(request, 'adv/advertisement.html', context)
